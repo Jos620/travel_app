9a9f067d55feae3bcb1f7628cc2df062f97be874
Mateus Ito <mateusito10@gmail.com>
2022-01-19 23:28:05 -0300

    Change tab indicator to circle

--- lib/pages/nav_pages/home_page.dart
+++ lib/pages/nav_pages/home_page.dart
@@ -65,6 +65,10 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                     unselectedLabelColor: Colors.grey,
                     isScrollable: true,
                     labelPadding: const EdgeInsets.only(left: 20, right: 20),
+                    indicator: const CircleTabIndicator(
+                      color: AppColors.mainColor,
+                      radius: 3,
+                    ),
                     tabs: const [
                       Tab(
                         text: 'Places',
@@ -97,5 +101,41 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         ),
       ),
     );
+  }
+}
+
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  final double radius;
+
+  const CircleTabIndicator({required this.color, required this.radius});
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _CircleTabIndicatorPainter(
+      color: color,
+      radius: radius,
+    );
+  }
+}
+
+class _CircleTabIndicatorPainter extends BoxPainter {
+  final Color color;
+  final double radius;
+
+  _CircleTabIndicatorPainter({required this.color, required this.radius});
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    Paint _paint = Paint();
+    _paint.color = color;
+    _paint.isAntiAlias = true;
+
+    final Offset circleOffset = Offset(
+      configuration.size!.width / 2 - radius / 2,
+      configuration.size!.height - radius - 4,
+    );
+
+    canvas.drawCircle(offset + circleOffset, radius, _paint);
   }
 }
